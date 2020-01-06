df8378fc30e5fb384e80bdee32661720ce6dbec0
Simon <simonre149@gmail.com>
2020-01-06 09:50:22 +0100

    Changement de bd

--- src/Migrations/Version20200106084549.php
+++ src/Migrations/Version20200106084549.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191118082910 extends AbstractMigration
+final class Version20200106084549 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,9 @@ final class Version20191118082910 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE classroom ADD teacher_id INT DEFAULT NULL');
+        $this->addSql('CREATE TABLE classroom (id INT AUTO_INCREMENT NOT NULL, teacher_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, start_at DATETIME NOT NULL, end_at DATETIME NOT NULL, student_group VARCHAR(255) NOT NULL, INDEX IDX_497D309D41807E1D (teacher_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:simple_array)\', student_group VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE classroom ADD CONSTRAINT FK_497D309D41807E1D FOREIGN KEY (teacher_id) REFERENCES user (id)');
-        $this->addSql('CREATE INDEX IDX_497D309D41807E1D ON classroom (teacher_id)');
     }
 
     public function down(Schema $schema) : void
@@ -33,7 +33,7 @@ final class Version20191118082910 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE classroom DROP FOREIGN KEY FK_497D309D41807E1D');
-        $this->addSql('DROP INDEX IDX_497D309D41807E1D ON classroom');
-        $this->addSql('ALTER TABLE classroom DROP teacher_id');
+        $this->addSql('DROP TABLE classroom');
+        $this->addSql('DROP TABLE user');
     }
 }
