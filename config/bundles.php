520e5dcfe58354139c59e102666f0fb92b6eab73
Simon <simonre149@gmail.com>
2020-01-08 17:14:00 +0100

    API log bundle removed

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -92,14 +92,5 @@ class SecurityController extends AbstractController
         ]);
     }
 
-    public function api_login(): JsonResponse
-    {
-        $user = $this->getUser();
-        return new JsonResponse([
-            'username' => $user->getUsername(),
-            'roles' => $user->getRoles(),
-        ]);
-    }
-
     public function logout(){}
 }
