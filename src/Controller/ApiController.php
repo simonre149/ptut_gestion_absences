7b956337d165f6d4dc69aaa62d8f445d41ff210f
sire <Simon.RE@etu.uca.fr>
2020-02-06 16:47:36 +0100

    Token start

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -10,83 +10,45 @@ use Symfony\Component\Validator\Constraints\Json;
 
 class ApiController extends AbstractController
 {
-    private $iv;
-    private $cipher;
-    private $tag;
-    private $key;
-
     public function tokenGeneration()
     {
         if (!$this->getUser()) return $this->redirectToRoute('login');
 
         $user = $this->getUser();
         $token = $user->getId() . "#" . $user->getUsername() . "#" . $user->getClassroomGroup() . "#" . $user->getName() . "#" . $user->getFirstname() . 'tok';
-        $crypted_token = $token;
-
-        /*
-        $cipher = "aes-128-gcm";
-        $ivlen = openssl_cipher_iv_length($cipher);
-        $iv = openssl_random_pseudo_bytes($ivlen);
-        $key = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cipher));
-
-        $crypted_token = openssl_encrypt($token, $cipher, $key, 0, $iv, $tag);
-
-
-
-        $this->cipher = $cipher;
-        $this->iv = $iv;
-        $this->tag = $tag;
-        $this->key = $key;
-
-        dd($crypted_token);*/
 
         return $this->render('pages/generatetoken.html.twig', [
-            'crypted_token' => $crypted_token,
+            'token' => $token,
             'role' => $user->getRoles()
         ]);
     }
 
     public function tokenRefresh()
     {
-
         return $this->redirectToRoute('generate_token');
     }
 
     public function getUserFromToken(Request $request, UserRepository $userRepository)
     {
-        $crypted_token = $request->get('token');
+        $token = json_decode($request->getContent());
+        $token = $token->token;
 
-        /*
-         $decrypted_token = openssl_decrypt($crypted_token, $this->cipher, $this->key, $options=0, $this->iv, $this->tag);
-
-        if(strpos($crypted_token, ' '))
-        {
-            $crypted_token = str_replace(" ", "+", $crypted_token);
-        }
-
-        dump($crypted_token);
-        dd($decrypted_token);*/
-
-        $decrypted_token = $crypted_token; //A CHANGER
-        $user_id = $decrypted_token[0];
-
-        $user = $userRepository->findUserArrayById($user_id);
-
-        return new JsonResponse($user);
+        return new JsonResponse('Ton token est : '. $token);
     }
 
+    /*
     public function testApi(Request $request)
     {
-        //if ($request->isMethod('POST'))
-        //{
+        if ($request->isMethod('POST'))
+        {
             $data = json_decode($request->getContent());
             $data = $data->data;
 
             return new JsonResponse('Test ok pour : ' . $data);
-        //}
-        //else
-        //{
-            //return $this->redirectToRoute('home');
-        //}
-    }
+        }
+        else
+        {
+            return $this->redirectToRoute('home');
+        }
+    }*/
 }
